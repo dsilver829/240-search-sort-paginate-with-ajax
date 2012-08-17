02de4e6eb235e0d66bfc6a9a71e488ad9f8dd133
David Silver <dsilver@candidatemetrics.com>
2012-08-17 09:22:01 -0700

    update gems and config files

--- store/db/schema.rb
+++ store/db/schema.rb
@@ -1,8 +1,9 @@
-# This file is auto-generated from the current state of the database. Instead 
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your 
+# Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
@@ -16,8 +17,8 @@ ActiveRecord::Schema.define(:version => 20100823060835) do
     t.string   "name"
     t.decimal  "price"
     t.datetime "released_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
